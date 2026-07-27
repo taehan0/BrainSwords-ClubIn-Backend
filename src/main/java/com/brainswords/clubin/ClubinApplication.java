5f741d54a6a8c1688ff9304770d1e0ebaa126db5
taehan0 <taehan1000@naver.com>
2026-07-27 16:53:49 +0900

    chore: Spring Boot 프로젝트 초기 생성

---
+++ src/main/java/com/brainswords/clubin/ClubinApplication.java
@@ -0,0 +1,13 @@
+package com.brainswords.clubin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubinApplication.class, args);
+	}
+
+}
